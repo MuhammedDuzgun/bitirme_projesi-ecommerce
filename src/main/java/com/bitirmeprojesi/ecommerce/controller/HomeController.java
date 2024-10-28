b964d8f7f3e2c8f241baa03b64babcedd15e2a7a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-29 01:27:16 +0300

    create first controller and add home

--- src/main/java/com/bitirmeprojesi/ecommerce/controller/HomeController.java
+++ src/main/java/com/bitirmeprojesi/ecommerce/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bitirmeprojesi.ecommerce.controller;
 
+import com.bitirmeprojesi.ecommerce.response.ApiResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @GetMapping
-    public String home() {
-        return "home";
+    public ApiResponse home() {
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Home Page");
+        return apiResponse;
     }
 
 }
