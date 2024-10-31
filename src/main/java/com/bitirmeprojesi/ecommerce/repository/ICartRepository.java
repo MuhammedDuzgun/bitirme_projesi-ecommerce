e75eb0039cd93b83ca84170007c009e895b8eda9
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-31 21:42:12 +0300

    add findUserById method

--- src/main/java/com/bitirmeprojesi/ecommerce/repository/ICartRepository.java
+++ src/main/java/com/bitirmeprojesi/ecommerce/repository/ICartRepository.java
@@ -4,5 +4,5 @@ import com.bitirmeprojesi.ecommerce.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ICartRepository extends JpaRepository<Cart, Long> {
-
+    Cart findByUserId(Long userId);
 }
